fbb33104c0d66233d43d37a24c06cfd759080881
dahyunnss <hyeons.opy@gmail.com>
2024-10-18 14:50:36 +0900

    ljspeech_prepare_align.py

--- prepare_align.py
+++ prepare_align.py
@@ -8,10 +8,10 @@ from preprocessor import ljspeech, aishell3, libritts
 def main(config):
     if "LJSpeech" in config["dataset"]:
         ljspeech.prepare_align(config)
-    if "AISHELL3" in config["dataset"]:
-        aishell3.prepare_align(config)
-    if "LibriTTS" in config["dataset"]:
-        libritts.prepare_align(config)
+    # if "AISHELL3" in config["dataset"]:
+    #     aishell3.prepare_align(config)
+    # if "LibriTTS" in config["dataset"]:
+    #     libritts.prepare_align(config)
 
 
 if __name__ == "__main__":
